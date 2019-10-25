c8e9688e258f6f3dfd84400379dc847f4743f71b
Brady <bradystarcher@hotmail.com>
2019-10-25 11:42:33 -0400

    Re-added other models

---
+++ TimeSheetAppTemp/Models/Payroll.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeSheetAppTemp.Models
+{
+    public class Payroll
+    {
+        public int ID { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public User EmployeeID { get; set; }
+    }
+}
