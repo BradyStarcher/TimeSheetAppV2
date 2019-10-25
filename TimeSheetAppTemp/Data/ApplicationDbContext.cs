17b5145dd0ceda677da2e07de8b442c87d8a2625
Brady <bradystarcher@hotmail.com>
2019-10-25 11:31:45 -0400

    Login works now with Roles

---
+++ TimeSheetAppTemp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TimeSheetAppTemp.Models;
+
+namespace TimeSheetAppTemp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User, Role, String>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ TimeSheetAppTemp/Data/Migrations/20191025152402_ExtendedUsersAndRoles.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TimeSheetAppTemp.Data.Migrations
+{
+    public partial class ExtendedUsersAndRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDate",
+                table: "AspNetRoles",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "AspNetRoles",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Division",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    User = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Division", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Division_AspNetUsers_User",
+                        column: x => x.User,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Division_User",
+                table: "Division",
+                column: "User",
+                unique: true,
+                filter: "[User] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Division");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CreationDate",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "AspNetRoles");
+        }
+    }
+}
